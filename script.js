0aac758a8f590639854a800b6696ce965855b2a3
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-23 02:34:52 +0530

    making selection text button

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function escapeQuotes() {
     var output = input.replace(/'/g, "\\'");
     document.getElementById('output').innerHTML = output;
 }
+
+function copyToClipboard() {
+    var copyText = document.getElementById("output");
+    copyText.select();
+    document.execCommand("copy");
+    /* window.getSelection().removeAllRanges();
+    alert("Copied the text: " + copyText.value); */
+}
